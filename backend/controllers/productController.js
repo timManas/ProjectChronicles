8a6fb7c0c17a4b8a812d028ed1a0b43fb34e7151
Tim M <timothy.romero.manas@gmail.com>
2024-02-03 20:17:27 -0500

    Adding async handlers for request errors

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,15 +1,16 @@
 import Product from '../models/productModel.js'
+import asyncHandler from 'express-async-handler'
 import { ObjectId } from 'mongodb'
 
-const getAllProducts = async (req, res) => {
+const getAllProducts = asyncHandler(async (req, res) => {
   const ProductToSend = await Product.find({})
   res.send(ProductToSend)
-}
+})
 
-const getProduct = async (req, res) => {
+const getProduct = asyncHandler(async (req, res) => {
   console.log('id: ' + req.params.id)
   const singleProduct = await Product.findById(req.params.id)
   res.send(singleProduct)
-}
+})
 
 export { getAllProducts, getProduct }
