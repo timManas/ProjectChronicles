a3c54056d5c5a8928d6f360ba5ed9973e697efca
Tim M <timothy.romero.manas@gmail.com>
2024-03-05 21:18:46 -0500

    Adding Contact Page

--- frontend/src/screens/ChronicleListPage.jsx
+++ frontend/src/screens/ChronicleListPage.jsx
@@ -1,10 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import {
-  Box,
   Heading,
-  Image,
-  Text,
   Divider,
   Wrap,
   WrapItem,
@@ -12,9 +9,9 @@ import {
   Stack,
   SimpleGrid,
 } from '@chakra-ui/react'
-import EditorsStory from '../components/ListPage/EditorsPick'
-import BlogTags from '../components/ListPage/BlogTags'
-import BlogAuthor from '../components/ListPage/BlogAuthor'
+import EditorsStory from '../components/ListPage/EditorsPick.jsx'
+
+import ArticleCard from '../components/ListPage/ArticleCard.jsx'
 
 const ChronicleListPage = () => {
   const [products, setProduct] = useState([])
@@ -46,120 +43,19 @@ const ChronicleListPage = () => {
       <SimpleGrid columns={[2, null, 3]} spacing='40px'>
         <Wrap marginTop='5'>
           <WrapItem width={{ base: '100%' }}>
-            <Box w='100%'>
-              <Box borderRadius='lg' overflow='hidden'>
-                <Box textDecoration='none' _hover={{ textDecoration: 'none' }}>
-                  <Image
-                    transform='scale(1.0)'
-                    src={
-                      'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
-                    }
-                    alt='some text'
-                    objectFit='contain'
-                    width='100%'
-                    transition='0.3s ease-in-out'
-                    _hover={{
-                      transform: 'scale(1.05)',
-                    }}
-                  />
-                </Box>
-              </Box>
-              <BlogTags tags={['Engineering', 'Product']} marginTop={3} />
-              <Heading fontSize='xl' marginTop='2'>
-                <Text textDecoration='none' _hover={{ textDecoration: 'none' }}>
-                  Some blog title
-                </Text>
-              </Heading>
-              <Text as='p' fontSize='md' marginTop='2'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry&apos;s standard
-                dummy text ever since the 1500s, when an unknown printer took a
-                galley of type and scrambled it to make a type specimen book.
-              </Text>
-              <BlogAuthor
-                name='John Doe'
-                date={new Date('2021-04-06T19:01:27Z')}
-              />
-            </Box>
+            <ArticleCard />
           </WrapItem>
         </Wrap>
 
         <Wrap marginTop='5'>
           <WrapItem width={{ base: '100%' }}>
-            <Box w='100%'>
-              <Box borderRadius='lg' overflow='hidden'>
-                <Box textDecoration='none' _hover={{ textDecoration: 'none' }}>
-                  <Image
-                    transform='scale(1.0)'
-                    src={
-                      'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
-                    }
-                    alt='some text'
-                    objectFit='contain'
-                    width='100%'
-                    transition='0.3s ease-in-out'
-                    _hover={{
-                      transform: 'scale(1.05)',
-                    }}
-                  />
-                </Box>
-              </Box>
-              <BlogTags tags={['Engineering', 'Product']} marginTop={3} />
-              <Heading fontSize='xl' marginTop='2'>
-                <Text textDecoration='none' _hover={{ textDecoration: 'none' }}>
-                  Some blog title
-                </Text>
-              </Heading>
-              <Text as='p' fontSize='md' marginTop='2'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry&apos;s standard
-                dummy text ever since the 1500s, when an unknown printer took a
-                galley of type and scrambled it to make a type specimen book.
-              </Text>
-              <BlogAuthor
-                name='John Doe'
-                date={new Date('2021-04-06T19:01:27Z')}
-              />
-            </Box>
+            <ArticleCard />
           </WrapItem>
         </Wrap>
+
         <Wrap marginTop='5'>
           <WrapItem width={{ base: '100%' }}>
-            <Box w='100%'>
-              <Box borderRadius='lg' overflow='hidden'>
-                <Box textDecoration='none' _hover={{ textDecoration: 'none' }}>
-                  <Image
-                    transform='scale(1.0)'
-                    src={
-                      'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
-                    }
-                    alt='some text'
-                    objectFit='contain'
-                    width='100%'
-                    transition='0.3s ease-in-out'
-                    _hover={{
-                      transform: 'scale(1.05)',
-                    }}
-                  />
-                </Box>
-              </Box>
-              <BlogTags tags={['Engineering', 'Product']} marginTop={3} />
-              <Heading fontSize='xl' marginTop='2'>
-                <Text textDecoration='none' _hover={{ textDecoration: 'none' }}>
-                  Some blog title
-                </Text>
-              </Heading>
-              <Text as='p' fontSize='md' marginTop='2'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry&apos;s standard
-                dummy text ever since the 1500s, when an unknown printer took a
-                galley of type and scrambled it to make a type specimen book.
-              </Text>
-              <BlogAuthor
-                name='John Doe'
-                date={new Date('2021-04-06T19:01:27Z')}
-              />
-            </Box>
+            <ArticleCard />
           </WrapItem>
         </Wrap>
       </SimpleGrid>
@@ -173,81 +69,19 @@ const ChronicleListPage = () => {
       <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
         <Wrap marginTop='5'>
           <WrapItem width={{ base: '100%' }}>
-            <Box w='100%'>
-              <Box borderRadius='lg' overflow='hidden'>
-                <Box textDecoration='none' _hover={{ textDecoration: 'none' }}>
-                  <Image
-                    transform='scale(1.0)'
-                    src={
-                      'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
-                    }
-                    alt='some text'
-                    objectFit='contain'
-                    width='100%'
-                    transition='0.3s ease-in-out'
-                    _hover={{
-                      transform: 'scale(1.05)',
-                    }}
-                  />
-                </Box>
-              </Box>
-              <BlogTags tags={['Engineering', 'Product']} marginTop={3} />
-              <Heading fontSize='xl' marginTop='2'>
-                <Text textDecoration='none' _hover={{ textDecoration: 'none' }}>
-                  Some blog title
-                </Text>
-              </Heading>
-              <Text as='p' fontSize='md' marginTop='2'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry&apos;s standard
-                dummy text ever since the 1500s, when an unknown printer took a
-                galley of type and scrambled it to make a type specimen book.
-              </Text>
-              <BlogAuthor
-                name='John Doe'
-                date={new Date('2021-04-06T19:01:27Z')}
-              />
-            </Box>
+            <ArticleCard />
           </WrapItem>
         </Wrap>
 
         <Wrap marginTop='5'>
           <WrapItem width={{ base: '100%' }}>
-            <Box w='100%'>
-              <Box borderRadius='lg' overflow='hidden'>
-                <Box textDecoration='none' _hover={{ textDecoration: 'none' }}>
-                  <Image
-                    transform='scale(1.0)'
-                    src={
-                      'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
-                    }
-                    alt='some text'
-                    objectFit='contain'
-                    width='100%'
-                    transition='0.3s ease-in-out'
-                    _hover={{
-                      transform: 'scale(1.05)',
-                    }}
-                  />
-                </Box>
-              </Box>
-              <BlogTags tags={['Engineering', 'Product']} marginTop={3} />
-              <Heading fontSize='xl' marginTop='2'>
-                <Text textDecoration='none' _hover={{ textDecoration: 'none' }}>
-                  Some blog title
-                </Text>
-              </Heading>
-              <Text as='p' fontSize='md' marginTop='2'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry&apos;s standard
-                dummy text ever since the 1500s, when an unknown printer took a
-                galley of type and scrambled it to make a type specimen book.
-              </Text>
-              <BlogAuthor
-                name='John Doe'
-                date={new Date('2021-04-06T19:01:27Z')}
-              />
-            </Box>
+            <ArticleCard />
+          </WrapItem>
+        </Wrap>
+
+        <Wrap marginTop='5'>
+          <WrapItem width={{ base: '100%' }}>
+            <ArticleCard />
           </WrapItem>
         </Wrap>
       </Stack>
--- frontend/src/screens/ContactPage.jsx
+++ frontend/src/screens/ContactPage.jsx
@@ -1,20 +1,133 @@
-import { VStack, Heading, Text } from '@chakra-ui/react'
+import {
+  Container,
+  Flex,
+  Box,
+  Heading,
+  Text,
+  Button,
+  VStack,
+  Wrap,
+  WrapItem,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Textarea,
+} from '@chakra-ui/react'
+
+import { MdPhone, MdEmail, MdLocationOn, MdOutlineEmail } from 'react-icons/md'
+import { BsPerson } from 'react-icons/bs'
 
 const ContactPage = () => {
   return (
-    <>
-      <VStack paddingTop='40px' spacing='2' alignItems='flex-start'>
-        <Heading as='h2'>Contact Us</Heading>
-        <Text as='p' fontSize='lg'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          condimentum quam arcu, eu tempus tortor molestie at. Vestibulum
-          pretium condimentum dignissim. Vestibulum ultrices vitae nisi sed
-          imperdiet. Mauris quis erat consequat, commodo massa quis, feugiat
-          sapien. Suspendisse placerat vulputate posuere. Curabitur neque
-          tortor, mattis nec lacus non, placerat congue elit.
-        </Text>
-      </VStack>
-    </>
+    <Container maxW='full' mt={0} centerContent overflow='hidden'>
+      <Flex>
+        <Box
+          bg='#02054B'
+          color='white'
+          borderRadius='lg'
+          m={{ sm: 4, md: 16, lg: 10 }}
+          p={{ sm: 5, md: 5, lg: 16 }}
+        >
+          <Box p={4}>
+            <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
+              <WrapItem>
+                <Box>
+                  <Heading>Contact</Heading>
+                  <Text mt={{ sm: 3, md: 3, lg: 5 }} color='gray.500'>
+                    Get in Touch !
+                  </Text>
+                  <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
+                    <VStack pl={0} spacing={3} alignItems='flex-start'>
+                      <Button
+                        size='md'
+                        height='48px'
+                        width='200px'
+                        variant='ghost'
+                        color='#DCE2FF'
+                        _hover={{ border: '2px solid #1C6FEB' }}
+                        leftIcon={<MdPhone color='#1970F1' size='20px' />}
+                      >
+                        +91-988888888
+                      </Button>
+                      <Button
+                        size='md'
+                        height='48px'
+                        width='200px'
+                        variant='ghost'
+                        color='#DCE2FF'
+                        _hover={{ border: '2px solid #1C6FEB' }}
+                        leftIcon={<MdEmail color='#1970F1' size='20px' />}
+                      >
+                        hello@abc.com
+                      </Button>
+                      <Button
+                        size='md'
+                        height='48px'
+                        width='200px'
+                        variant='ghost'
+                        color='#DCE2FF'
+                        _hover={{ border: '2px solid #1C6FEB' }}
+                        leftIcon={<MdLocationOn color='#1970F1' size='20px' />}
+                      >
+                        Toronto, Canada
+                      </Button>
+                    </VStack>
+                  </Box>
+                </Box>
+              </WrapItem>
+              <WrapItem>
+                <Box bg='white' borderRadius='lg'>
+                  <Box m={8} color='#0B0E3F'>
+                    <VStack spacing={5}>
+                      <FormControl id='name'>
+                        <FormLabel>Name</FormLabel>
+                        <InputGroup borderColor='#E0E1E7'>
+                          <InputLeftElement pointerEvents='none'>
+                            <BsPerson color='gray.800' />
+                          </InputLeftElement>
+                          <Input type='text' size='md' />
+                        </InputGroup>
+                      </FormControl>
+                      <FormControl id='name'>
+                        <FormLabel>Mail</FormLabel>
+                        <InputGroup borderColor='#E0E1E7'>
+                          <InputLeftElement pointerEvents='none'>
+                            <MdOutlineEmail color='gray.800' />
+                          </InputLeftElement>
+                          <Input type='text' size='md' />
+                        </InputGroup>
+                      </FormControl>
+                      <FormControl id='name'>
+                        <FormLabel>Message</FormLabel>
+                        <Textarea
+                          borderColor='gray.300'
+                          _hover={{
+                            borderRadius: 'gray.300',
+                          }}
+                          placeholder='message'
+                        />
+                      </FormControl>
+                      <FormControl id='name' float='right'>
+                        <Button
+                          variant='solid'
+                          bg='#0D74FF'
+                          color='white'
+                          _hover={{}}
+                        >
+                          Send Message
+                        </Button>
+                      </FormControl>
+                    </VStack>
+                  </Box>
+                </Box>
+              </WrapItem>
+            </Wrap>
+          </Box>
+        </Box>
+      </Flex>
+    </Container>
   )
 }
 
