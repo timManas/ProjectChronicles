3261766ef5ebd26419fd546232c22575bc122c2b
Tim M <timothy.romero.manas@gmail.com>
2024-03-09 10:57:14 -0500

    Remove unneccessary junk

--- frontend/src/screens/ContactPage.jsx
+++ frontend/src/screens/ContactPage.jsx
@@ -31,12 +31,12 @@ const ContactPage = () => {
             <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
               <WrapItem>
                 <Box>
-                  <Heading>Advertise With Us</Heading>
+                  <Heading align='center'>Work with Us ! </Heading>
 
                   <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
                     <VStack pl={0} spacing={3} alignItems='center'>
                       <Text color='gray.500'>
-                        <p>Looking to Collab ? Send us a message ! </p>
+                        <p>Looking to Collab ? Send us an email ! </p>
                         <p> We are always looking to work with the </p>
                         <p>best and upcoming people in the industry </p>
                       </Text>
@@ -73,52 +73,6 @@ const ContactPage = () => {
                       >
                         Toronto, Canada
                       </Button>
-                    </VStack>
-                  </Box>
-                </Box>
-              </WrapItem>
-              <WrapItem>
-                <Box bg='white' borderRadius='lg'>
-                  <Box m={8} color='#0B0E3F'>
-                    <VStack spacing={5}>
-                      <FormControl id='name'>
-                        <FormLabel>Your Name</FormLabel>
-                        <InputGroup borderColor='#E0E1E7'>
-                          <InputLeftElement pointerEvents='none'>
-                            <BsPerson color='gray.800' />
-                          </InputLeftElement>
-                          <Input type='text' size='md' />
-                        </InputGroup>
-                      </FormControl>
-                      <FormControl id='name'>
-                        <FormLabel>Mail</FormLabel>
-                        <InputGroup borderColor='#E0E1E7'>
-                          <InputLeftElement pointerEvents='none'>
-                            <MdOutlineEmail color='gray.800' />
-                          </InputLeftElement>
-                          <Input type='text' size='md' />
-                        </InputGroup>
-                      </FormControl>
-                      <FormControl id='name'>
-                        <FormLabel>Message</FormLabel>
-                        <Textarea
-                          borderColor='gray.300'
-                          _hover={{
-                            borderRadius: 'gray.300',
-                          }}
-                          placeholder='message'
-                        />
-                      </FormControl>
-                      <FormControl id='name' float='right'>
-                        <Button
-                          variant='solid'
-                          bg='#0D74FF'
-                          color='white'
-                          _hover={{}}
-                        >
-                          Send Message
-                        </Button>
-                      </FormControl>
                     </VStack>
                   </Box>
                 </Box>
