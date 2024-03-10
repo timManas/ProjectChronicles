52bc53e55d388f8ce3dc53ed42390f69bd792608
Tim M <timothy.romero.manas@gmail.com>
2024-03-09 19:32:05 -0500

    Modify vite config for proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   parser: '@babel/eslint-parser',
+  server: {
+    // proxy requests prefixed '/api' and '/uploads'
+    proxy: {
+      '/api': 'http://localhost:5000',
+      '/uploads': 'http://localhost:5000',
+    },
+  },
 })
