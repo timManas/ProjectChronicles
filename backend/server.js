433396e231dda6275d59ec61411ee9c0a444dbdd
Tim M <timothy.romero.manas@gmail.com>
2024-03-09 19:45:09 -0500

    Adding base configuration

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,21 @@ app.use(cors())
 
 app.use('/api/articles', articleRoutes)
 
+const __dirname = path.resolve()
+if (process.env.NODE_ENV === 'production') {
+  app.use('/uploads', express.static('/var/data/uploads'))
+  app.use(express.static(path.join(__dirname, './client/dist')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  )
+} else {
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 app.listen(PORT, () => {
   console.log(`Server running in Port: ${PORT}`)
 })
