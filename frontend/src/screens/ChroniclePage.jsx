552a444975caaf7b5f5f5e9747904d8d6f15b49e
Tim M <timothy.romero.manas@gmail.com>
2025-01-06 19:47:15 -0500

    Change Image position

--- frontend/src/screens/ChroniclePage.jsx
+++ frontend/src/screens/ChroniclePage.jsx
@@ -45,21 +45,10 @@ const ChroniclePage = () => {
       {!isLoading && (
         <Container maxW={'7xl'}>
           <SimpleGrid
-            columns={{ base: 1, lg: 2 }}
-            spacing={{ base: 8, md: 10 }}
+            columns={{ base: 1, lg: 1 }}
+            spacing={{ base: 1, md: 10 }}
             py={{ base: 18, md: 24 }}
           >
-            <Flex>
-              <Image
-                rounded={'md'}
-                alt={'product image'}
-                src={article.image}
-                fit={'cover'}
-                align={'center'}
-                w={'100%'}
-                h={{ base: '100%', sm: 'ß400px', lg: '500px' }}
-              />
-            </Flex>
             <Stack spacing={{ base: 6, md: 10 }}>
               <Box as={'header'}>
                 <Heading
@@ -77,6 +66,35 @@ const ChroniclePage = () => {
                   {article.articlePage.price}
                 </Text>
               </Box>
+            </Stack>
+            <Flex>
+              <Image
+                rounded={'md'}
+                alt={'product image'}
+                src={article.image}
+                fit={'cover'}
+                align={'center'}
+                w={'100%'}
+                h={{ base: '100%', sm: '400px', lg: '500px' }}
+              />
+            </Flex>
+            <Stack spacing={{ base: 6, md: 10 }}>
+              {/* <Box as={'header'}>
+                <Heading
+                  lineHeight={1.1}
+                  fontWeight={600}
+                  fontSize={{ base: '2xl', sm: '4xl', lg: '5xl' }}
+                >
+                  {article.title}
+                </Heading>
+                <Text
+                  color={useColorModeValue('gray.900', 'gray.400')}
+                  fontWeight={300}
+                  fontSize={'2xl'}
+                >
+                  {article.articlePage.price}
+                </Text>
+              </Box> */}
 
               <Stack
                 spacing={{ base: 4, sm: 6 }}
@@ -121,6 +139,17 @@ const ChroniclePage = () => {
                     </List>
                   </SimpleGrid>
                 </Box>
+
+                <Image
+                  rounded={'md'}
+                  alt={'product image'}
+                  src={article.image}
+                  fit={'cover'}
+                  align={'center'}
+                  w={'100%'}
+                  h={{ base: '100%', sm: '400px', lg: '500px' }}
+                />
+
                 <Box>
                   <Text
                     fontSize={{ base: '16px', lg: '18px' }}
